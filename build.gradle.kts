b545dba3250c7ccb23f47c24e33421a4d1e525f7
Sana Ebadi <sanaebadi97@gmail.com>
2020-10-16 21:27:13 +0330

    init commit (I changed gradle to Kotlin DSL )

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    val kotlin_version by extra("1.4.10")
+    repositories {
+        google()
+        jcenter()
+
+    }
+    dependencies {
+        classpath (BuildPlugins.androidGradlePlugin)
+        classpath (BuildPlugins.kotlinGradlePlugin)
+        classpath (BuildPlugins.safeArgsPlugin)
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+
+    }
+}
+
+tasks.register("clean").configure {
+    delete("build")
+}
