3b686a1d79a0bf0638125122f1ae9a472e99a995
Sana Ebadi <sanaebadi97@gmail.com>
2020-10-18 11:33:07 +0330

    added all build gradls kts

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,13 +1,33 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-    id 'java-library'
-    id 'kotlin'
+    id(BuildPlugins.javaLibraryPlugin)
+    id(BuildPlugins.kotlinPlugin)
+    id(BuildPlugins.kaptPlugin)
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_7
-    targetCompatibility = JavaVersion.VERSION_1_7
-}
 
 dependencies {
-    implementation "org.jetbrains.kotlin:kotlin-stdlib:$kotlin_version"
+    implementation(Libraries.kotlinStdLib)
+    implementation(RXLibraries.rxAndroid)
+    implementation(RXLibraries.rxJava)
+    implementation(Libraries.arrow)
+
+    implementation(DaggerLib.dagger)
+    implementation(DaggerLib.daggerSupport)
+    kapt(DaggerLib.daggerCompiler)
+    kapt(DaggerLib.daggerProcessor)
+
+    implementation(RXLibraries.rxAndroid)
+    implementation(RXLibraries.rxJava)
+    implementation(RXLibraries.rxKotlin)
+
+    testImplementation(TestLibraries.junit4)
+    testImplementation(TestLibraries.mockitoKotlin)
+}
+
+
+// compile bytecode to java 8 (default is java 6)
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
 }
