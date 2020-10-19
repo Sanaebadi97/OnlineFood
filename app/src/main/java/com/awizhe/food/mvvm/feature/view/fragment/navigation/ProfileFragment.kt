be222db8eb39a0fa9196bc282e488391db66f826
Sana Ebadi <sanaebadi97@gmail.com>
2020-10-19 14:54:42 +0330

    added ticket notes and fields

--- app/src/main/java/com/awizhe/food/mvvm/feature/view/fragment/navigation/ProfileFragment.kt
+++ app/src/main/java/com/awizhe/food/mvvm/feature/view/fragment/navigation/ProfileFragment.kt
@@ -1,4 +1,4 @@
-package com.awizhe.food.mvvm.feature.view.fragment
+package com.awizhe.food.mvvm.feature.view.fragment.navigation
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [CardFragment.newInstance] factory method to
+ * Use the [ProfileFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class CardFragment : Fragment() {
+class ProfileFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class CardFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_card, container, false)
+        return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
     companion object {
@@ -45,12 +45,12 @@ class CardFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment CardFragment.
+         * @return A new instance of fragment ProfileFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            CardFragment().apply {
+            ProfileFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
