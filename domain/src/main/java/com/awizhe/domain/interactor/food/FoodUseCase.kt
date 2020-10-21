28c3f491b6368ba788b20ca535d3bdce9b3bbb7a
Sana Ebadi <sanaebadi97@gmail.com>
2020-10-21 16:55:33 +0330

    completed domain layer

--- domain/src/main/java/com/awizhe/domain/interactor/food/FoodUseCase.kt
+++ domain/src/main/java/com/awizhe/domain/interactor/food/FoodUseCase.kt
@@ -1,3 +1,21 @@
 package com.awizhe.domain.interactor.food
 
-class FoodUseCase
+import com.awizhe.domain.model.food.FoodList
+import com.awizhe.domain.repository.food.FoodRepository
+import info.sanaebadi.domain.executor.PostExecutionThread
+import info.sanaebadi.domain.executor.ThreadExecutor
+import info.sanaebadi.domain.interactor.base.SingleUseCase
+import io.reactivex.Single
+import javax.inject.Inject
+
+class FoodUseCase @Inject constructor(
+    private val foodRepository: FoodRepository,
+    threadExecutor: ThreadExecutor,
+    postExecutionThread: PostExecutionThread,
+) : SingleUseCase<FoodList, String>(threadExecutor, postExecutionThread) {
+
+    override fun buildUseCaseSingle(params: String): Single<FoodList> {
+        return foodRepository.getFood()
+    }
+
+}
