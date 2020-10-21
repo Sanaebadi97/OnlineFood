2e3627ac202b5c4c30443a7b97dddc118f0370c5
Sana Ebadi <sanaebadi97@gmail.com>
2020-10-21 15:49:42 +0330

    added bottom navigation to navigation component

--- app/src/main/java/com/awizhe/food/mvvm/feature/view/MainActivity.kt
+++ app/src/main/java/com/awizhe/food/mvvm/feature/view/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.awizhe.food.mvvm.feature.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.awizhe.food.R
+import com.awizhe.food.databinding.ActivityMainBinding
+
+private lateinit var navController: NavController
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view: View = binding.root
+        setContentView(view)
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        binding.navigationView.setupWithNavController(navController)
+
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
     }
 }
