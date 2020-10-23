d0e2ff48baf95b15e400f858610ec31b11a82314
Sana Ebadi <sanaebadi97@gmail.com>
2020-10-23 21:42:38 +0330

    remove drawer layout

--- app/src/main/java/com/awizhe/food/mvvm/feature/view/MainActivity.kt
+++ app/src/main/java/com/awizhe/food/mvvm/feature/view/MainActivity.kt
@@ -1,34 +1,30 @@
 package com.awizhe.food.mvvm.feature.view
 
 import android.os.Bundle
-import android.view.Gravity
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.awizhe.food.R
 import com.awizhe.food.databinding.ActivityMainBinding
 
-private lateinit var navController: NavController
 private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
+    private val navController by lazy {
+        Navigation.findNavController(this, R.id.nav_host_fragment)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view: View = binding.root
         setContentView(view)
 
-        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
         binding.navigationView.setupWithNavController(navController)
-
-        binding.mainToolbar.imageMenu.setOnClickListener {
-
-            binding.drawerLayout.openDrawer(Gravity.RIGHT)
-
-        }
 
 
     }
@@ -37,8 +33,4 @@ class MainActivity : AppCompatActivity() {
         return NavigationUI.navigateUp(navController, null)
     }
 
-    override fun onBackPressed() {
-        binding.drawerLayout.closeDrawer(Gravity.RIGHT)
-        super.onBackPressed()
-    }
 }
